0badf63dcd2e41605b92d0abe56d23f0a7b2c9b5
AbhayKumar1415 <abhay.kumar@qed42.com>
2019-01-24 22:51:04 +0530

    Added google maps.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild, NgZone, ElementRef} from '@angular/core';
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  GoogleMapOptions,
+  CameraPosition,
+  MarkerOptions,
+  Marker,
+  Environment
+} from '@ionic-native/google-maps';
+import {Geolocation} from '@ionic-native/geolocation/ngx';
+declare var google: any;
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+  mapOptions: any;
+  location = {lat: null, lng: null};
+  markerOptions: any = {position: null, map: null, title: null};
+  marker: any;
+  apiKey: any = 'AIzaSyCCh36EiMSjGZzqyBjNqi2FaaYpowZ-P7E';
+  ionViewDidLoad() {
+  }
+  constructor(public zone: NgZone, public geolocation: Geolocation) {
+    const script = document.createElement('script');
+      script.id = 'googleMap';
+      if (this.apiKey) {
+          script.src = 'https://maps.googleapis.com/maps/api/js?key=' + this.apiKey;
+      } else {
+          script.src = 'https://maps.googleapis.com/maps/api/js?key=';
+      }
+      document.head.appendChild(script);
+      this.geolocation.getCurrentPosition().then((position) =>  {
+          this.location.lat = position.coords.latitude;
+          this.location.lng = position.coords.longitude;
+      });
+      /*Map options*/
+      this.mapOptions = {
+          center: this.location,
+          zoom: 3,
+          mapTypeControl: false
+      };
+      setTimeout(() => {
+          this.map = new google.maps.Map(this.mapElement.nativeElement, this.mapOptions);
+          this.markerOptions.position = this.location;
+          this.markerOptions.map = this.map;
+          this.markerOptions.title = 'My Location';
+          this.marker = new google.maps.Marker(this.markerOptions);
+      }, 3000);
+  }
 }
